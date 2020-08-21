dc5014638d8c1bf332c15791566a6689c2d52bdb
Alex Miller <alex.k.miller@gmail.com>
2020-08-21 13:14:15 -0400

    Faster

--- qb_country_filter.R
+++ qb_country_filter.R
@@ -47,73 +47,25 @@ drop = names(trans)[which(startsWith(names(trans),"activity_"))]
 drop = c(drop,"X_version_","title_lang","id","reporting_org_secondary_reporter")
 trans[,drop] = NULL
 
-big_url = paste0(
-  "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(",
-  recipient_country,
-  ") AND activity_date_start_actual_f:[",
-  start_date,
-  "T00:00:00Z TO *]&wt=csv&tr=activity-csv.xsl&rows=100000"
-)
 
-act_filename = paste0(recipient_country,"_act1.csv")
-
-if(!file.exists(act_filename)){
-  download.file(
-    URLencode(big_url),
-    destfile=act_filename
+if(!file.exists(paste0(recipient_country,"_activities.RData"))){
+  big_url = paste0(
+    "https://iatidatastore.iatistandard.org/search/activity?q=recipient_country_code:(",
+    recipient_country,
+    ") AND activity_date_start_actual_f:[",
+    start_date,
+    "T00:00:00Z TO *]&wt=csv&tr=activity-csv.xsl&rows=100000"
   )
-}
-
-act <- read.table(
-  act_filename,
-  header=T,
-  sep=",",
-  quote=c("\""),
-  na.strings="",
-  stringsAsFactors=FALSE,
-  flush=T,
-  fill=T
-)
-
-trans_ids = unique(trans$iati_identifier)
-
-act = subset(act,iati_identifier %in% trans_ids)
-
-act_ids = unique(act$iati_identifier)
-missing_ids = setdiff(trans_ids,act_ids)
-
-search_terms = missing_ids
-
-search_fields = c(
-  "iati_identifier"
-)
-
-search_query = ""
-search_grid = expand.grid(search_fields, search_terms, stringsAsFactors = F)
-search_strs = paste0(search_grid$Var1, ':"', search_grid$Var2, '"')
-chunk_seq = round(seq(1,length(search_strs),length.out=50))
-
-
-act_list = list()
-for(i in 1:(length(chunk_seq)-1)){
-  message(i)
-  start_i = chunk_seq[i]
-  end_i = chunk_seq[i+1]
-  chunk_strs = search_strs[start_i:end_i]
-  search_query = paste0(chunk_strs,collapse=" OR ")
-  search_url = paste0(
-    'https://iatidatastore.iatistandard.org/search/activity?q=(',
-    search_query,
-    ')&wt=csv&tr=activity-csv.xsl&rows=10000'
-  )
-  act_filename = paste0(recipient_country,"_act",i+1,".csv")
+  
+  act_filename = paste0(recipient_country,"_act1.csv")
+  
   if(!file.exists(act_filename)){
     download.file(
-      URLencode(search_url),
+      URLencode(big_url),
       destfile=act_filename
     )
   }
-  tmp <- read.table(
+  act <- read.table(
     act_filename,
     header=T,
     sep=",",
@@ -123,9 +75,65 @@ for(i in 1:(length(chunk_seq)-1)){
     flush=T,
     fill=T
   )
-  act_list[[i]] = tmp
+  
+  trans_ids = unique(trans$iati_identifier)
+  
+  act = subset(act,iati_identifier %in% trans_ids)
+  
+  act_ids = unique(act$iati_identifier)
+  missing_ids = setdiff(trans_ids,act_ids)
+  
+  search_terms = missing_ids
+  
+  search_fields = c(
+    "iati_identifier"
+  )
+  
+  search_query = ""
+  search_grid = expand.grid(search_fields, search_terms, stringsAsFactors = F)
+  search_strs = paste0(search_grid$Var1, ':"', search_grid$Var2, '"')
+  chunk_seq = round(seq(1,length(search_strs),length.out=200))
+  
+  
+  act_list = list()
+  for(i in 1:(length(chunk_seq)-1)){
+    message(i)
+    start_i = chunk_seq[i]
+    end_i = chunk_seq[i+1]
+    chunk_strs = search_strs[start_i:end_i]
+    search_query = paste0(chunk_strs,collapse=" OR ")
+    search_url = paste0(
+      'https://iatidatastore.iatistandard.org/search/activity?q=(',
+      search_query,
+      ')&wt=csv&tr=activity-csv.xsl&rows=10000'
+    )
+    act_filename = paste0(recipient_country,"_act",i+1,".csv")
+    if(!file.exists(act_filename)){
+      download.file(
+        URLencode(search_url),
+        destfile=act_filename
+      )
+    }
+    tmp <- fread(act_filename,colClasses = c(rep("character", 344)))
+    act_list[[i]] = tmp
+  }
+  
+  act_addition = rbindlist(act_list,fill=T)
+  act = rbind(act,act_addition,fill=T)
+  save(act,file=paste0(recipient_country,"_activities.RData"))
+}else{
+  load(paste0(recipient_country,"_activities.RData"))
 }
 
-act_addition = rbindlist(act_list)
-act = rbind(act,act_addition)
-save(act,file=paste0(recipient_country,"_activities.RData"))
+setdiff(unique(act$iati_identifier),unique(trans$iati_identifier))
+setdiff(unique(trans$iati_identifier),unique(act$iati_identifier))
+
+drop = names(act)[which(endsWith(names(act),"_xml"))]
+drop = c(drop,"_version_","X_version_","id")
+act[,drop] = NULL
+overlapping_names = intersect(names(act),names(trans))
+overlapping_names = overlapping_names[which(overlapping_names!="iati_identifier")]
+act[,overlapping_names] = NULL
+
+all = merge(trans,act,by="iati_identifier")
+write_excel_csv(all,paste0(recipient_country,"_transactions_merge_utf8.csv"),na="")
